00b251d9f6c29b7960a68fd72eb7abf74b64f48d
shadowrezi <urijozimko4@gmail.com>
2025-07-14 23:01:53 +0300

    1

--- app.py
+++ app.py
@@ -161,6 +161,5 @@ def profile():
     )
 
 
-#if __name__ == '__main__':
-#    app.run(host='0.0.0.0', port=5000, debug=True)
- 
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000, debug=True)
