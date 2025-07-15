0eb44a67bde8c397c8409f090006ca0ef55903b0
shadowrezi <urijozimko4@gmail.com>
2025-07-15 16:05:39 +0300

    1

--- app.py
+++ app.py
@@ -83,7 +83,8 @@ def buy_product(product_id):
     return redirect(product.payment_url)
 
 
-@app.route('/payment_success/<int:product_id>')
+@app.route('/payment_success/<int:product_id>', methods=['POST', 'GET'])
+@login_required
 def payment_success(product_id: int):
     product = Product.query.get_or_404(product_id)
     purchase = Purchase(user_id=current_user.id, product_id=product.id)
