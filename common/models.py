b5212e7f012a5e2113cb0d805a233ac5ff2cc925
shadowrezi <urijozimko4@gmail.com>
2025-07-29 00:39:36 +0300

    1

--- common/models.py
+++ common/models.py
@@ -9,6 +9,7 @@ class User(db.Model, UserMixin):
     username = db.Column(db.String(64), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(128), nullable=False)
+    telegram_id = db.Column(db.Integer(12), unique=True)
     purchases = db.relationship('Purchase', backref='user', lazy=True)
     
 
